c64d29b6b496956a2eda057c8bebc9f14298dac9
prakash-15 <129299282+prakash-15@users.noreply.github.com>
2023-08-03 16:17:55 +0530

    Add files via upload

---
+++ Demohello/src/main/java/com/example/Demohello/DemohelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemohelloApplication.class, args);
+	}
+
+}
